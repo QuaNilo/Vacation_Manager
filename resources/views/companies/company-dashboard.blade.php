ea5a1c0d4d687bd80800153932484de8182dcce4
QuaNilo <anyonept911@gmail.com>
2024-06-16 18:42:17 +0100

    Added company's name to dashboard

--- resources/views/companies/company-dashboard.blade.php
+++ resources/views/companies/company-dashboard.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <div id="main_div" class="flex flex-col h-screen">
+        <h1 class="text-center text-5xl font-bold m-8">{{auth()->user()->company()->first()->name}}</h1>
         <div id="stats_div" class="flex space-x-4 mb-12">
             <x-statistics-card  title="Whatever" value="Value"/>
             <x-statistics-card  title="Whatever" value="Value"/>
@@ -12,7 +13,6 @@
             </div>
             <div id="teams_table" class="flex-grow">
                 <livewire:teams-table/>
-
             </div>
         </div>
     </div>
